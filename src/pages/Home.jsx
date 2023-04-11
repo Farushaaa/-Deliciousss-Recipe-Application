3eff96367ba6a08bac0c3f983c4b839dc206deb8
Farusha <fariko2002@mail.ru>
2023-04-11 17:12:31 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Popular from "../components/Popular";
+import Veggie from "../components/Veggie";
+import React from "react";
+import { motion } from "framer-motion";
+const Home = () => {
+  return (
+    <motion.div>
+      <Veggie />
+      <Popular />
+    </motion.div>
+  );
+};
+
+export default Home;
